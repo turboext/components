4d9d71c4d09be7c176f00c880d1949c1117f05f9
mviktorov <nkviz40k@yandex.ru>
2019-03-26 14:51:31 +0300

    https

--- server/getHtml.ts
+++ server/getHtml.ts
@@ -2,7 +2,7 @@ import rp = require('request-promise');
 
 // @ts-ignore broken typings
 export const getHtml = (xmlData: string): Promise<string> => rp({
-    uri: 'turbo-components.yandex.net',
+    uri: 'https://turbo-components.yandex.net',
     method: 'POST',
     headers: {
         'content-type': 'application/json'
