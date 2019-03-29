12fdad2b30b2f87bf5f71996ac1b41c54b7c38bb
werreour <werreour@yandex.ru>
2019-03-29 14:48:32 +0300

    добавить кэширование запросов

--- server/getHtml.ts
+++ server/getHtml.ts
@@ -1,11 +1,27 @@
 import rp = require('request-promise');
+import { createHash } from 'crypto';
 
-// @ts-ignore broken typings
-export const getHtml = (xmlData: string): Promise<string> => rp({
-    uri: 'https://turbo-components.yandex.net',
-    method: 'POST',
-    headers: {
-        'content-type': 'application/json'
-    },
-    body: JSON.stringify({ xml: xmlData })
-});
+// Save xml data not to make (new) requests
+const savedXmls = {};
+
+export const getHtml = (xmlData: string): Promise<string> => {
+    const hash = createHash('md5').update(xmlData)
+        .digest('hex');
+    if (savedXmls[hash]) {
+        return Promise.resolve(savedXmls[hash]);
+    }
+
+    // @ts-ignore broken typings
+    return rp({
+        uri: 'https://turbo-components.yandex.net',
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({ xml: xmlData })
+    })
+        .then(html => {
+            savedXmls[hash] = html;
+            return html;
+        });
+};
