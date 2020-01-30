409f9028a6f91056c81ca9184c369a5f98ebdd3b
a.karelin <a.karelin@rambler-co.ru>
2020-01-30 18:00:40 +0300

    feat(components): add capirs advertisement component

---
+++ components/ExtCapirs/ExtCapirs.tsx
@@ -0,0 +1,153 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+import { camelCase } from 'camel-case';
+
+
+import { IWidgetParams, MessageType } from './commonInterfaces';
+import { inlineScript, GLOBAL_CALLBACKS_PROPERTY } from './inlineScript';
+import { ExtEmbed } from '../ExtEmbed/ExtEmbed';
+
+import './ExtCapirs.scss';
+
+interface IState {
+    htmlString: string | null;
+    loadingState: LoadingState;
+    height: number;
+    width: number | null;
+}
+
+interface IExtCapirsProps {
+    'data-begun-auto-pad': number;
+    'data-begun-block-id': number;
+    'data-json'?: Record<string, string | number>;
+}
+
+export interface IMessageData {
+    type: string;
+    height?: number;
+    width?: number;
+}
+
+const DEFAULT_HEIGHT = 320;
+
+enum LoadingState {
+    inProgress = 'inProgress',
+    succeed = 'succeed',
+    failed = 'failed'
+}
+
+interface IMessageHandlerArguments {
+    messageData: IMessageData;
+    origin: MessageEvent['origin'];
+    source: Window;
+}
+
+export class ExtCapirs extends React.PureComponent<IExtCapirsProps, IState> {
+    public readonly state: IState = {
+        htmlString: null,
+        loadingState: LoadingState.inProgress,
+        height: DEFAULT_HEIGHT,
+        width: null
+    };
+
+    private messagesHandlersMap = {
+        [MessageType.loadingSucceed]: ({ messageData }: IMessageHandlerArguments) =>
+            /* eslint-disable-next-line */
+            this.loadingSucceedHandler(messageData),
+        [MessageType.loadingFailed]: () => this.loadingFailedHandler(),
+        [MessageType.getLocation]: ({ origin, source }: IMessageHandlerArguments) => this.sendLocation(source, origin)
+    };
+
+    public componentDidMount(): void {
+        if (typeof window === 'undefined') {
+            return;
+        }
+
+        window.addEventListener('message', event => this.postMessageHandler(event));
+        this.composeHtmlString();
+    }
+
+    public render(): React.ReactNode {
+        return this.state.loadingState === LoadingState.failed ? null : (
+            <ExtEmbed
+                html={this.state.htmlString || ''}
+                iframeClass={this.state.width ? '' : 'ext-embed__ext-capirs_fill'}
+                iframeHeight={this.state.height.toString()}
+                iframeWidth={this.state.width === null ? '' : this.state.width.toString()}
+                isLoaded={this.state.loadingState === LoadingState.succeed}
+            />
+        );
+    }
+
+    private composeHtmlString(): void {
+        if (typeof window !== 'undefined') {
+            // Преобразуем все пришедшие data-пропсы в параметры виджета
+            const widgetParams: IWidgetParams = Object.keys(this.props)
+                .reduce((agr, propName) => {
+                    if (propName.indexOf('data-') === 0) {
+                        const camelCasedName = camelCase(propName.slice(5));
+                        agr[camelCasedName] = this.props[propName];
+                    }
+
+                    return agr;
+                }, {} as unknown as IWidgetParams);
+
+            const html = (
+                <>
+                    <div className="capirs-container" />
+                    <script
+                        /* eslint-disable-next-line */
+                        dangerouslySetInnerHTML={{ __html:
+                        `(${inlineScript.toString()})` +
+                        `(window,document,'${GLOBAL_CALLBACKS_PROPERTY}',${JSON.stringify(widgetParams)})` }}
+                    />
+                </>
+            );
+
+            const tempDiv = document.createElement('div');
+            render(html, tempDiv, () => {
+                this.setState({ htmlString: tempDiv.innerHTML });
+            });
+        }
+    }
+
+    private postMessageHandler(event: MessageEvent): void {
+        const needToProcessMessage =
+      event.data &&
+      event.data.message &&
+      Object.prototype.hasOwnProperty.call(
+          this.messagesHandlersMap,
+          event.data.message
+      );
+
+        if (needToProcessMessage) {
+            this.messagesHandlersMap[event.data.message]({
+                origin: event.origin,
+                source: event.source,
+                messageData: event.data
+            });
+        }
+    }
+
+    private loadingSucceedHandler(messageData: IMessageData): void {
+        this.setState({
+            loadingState: LoadingState.succeed,
+            height: messageData.height || DEFAULT_HEIGHT,
+            width: messageData.width || null
+        });
+    }
+
+    private loadingFailedHandler(): void {
+        this.setState({ loadingState: LoadingState.failed });
+    }
+
+    private sendLocation(source: Window, origin: MessageEvent['origin']): void {
+        if (typeof window !== 'undefined') {
+            source.postMessage({
+                message: MessageType.saveLocation,
+                href: location.href,
+                referrer: document.referrer
+            }, origin);
+        }
+    }
+}
---
+++ components/ExtCapirs/inlineScript.ts
@@ -0,0 +1,113 @@
+import { IWidgetParams } from './commonInterfaces';
+
+interface IBanner {
+    width: string;
+    height: string;
+}
+
+interface IFeed {
+    banners: { graph: { width: string; height: string }[] };
+}
+
+export const GLOBAL_CALLBACKS_PROPERTY = 'begun_callbacks';
+
+export function inlineScript(
+    window: Window,
+    document: Document,
+    globalCallbackProperty: string,
+    params: IWidgetParams
+): void {
+    window.addEventListener(
+        'message',
+        event => {
+            if (!event.data || event.data.message !== 'save-location') {
+                return;
+            }
+
+            window.context = window.context || {};
+            window.context.location = window.context.location || {};
+
+            // Save href and referrer in AMP-like fashion
+            window.context.location.href = window.context.location.href || event.data.href;
+            window.context.referrer = window.context.referrer || event.data.referrer;
+        },
+        false
+    );
+
+    // Request href and referrer from the top window
+    window.parent.postMessage({ message: 'get-location' }, '*');
+
+    function isResponsiveAd(dimension: string): boolean {
+        return dimension.indexOf('%') !== -1;
+    }
+
+    /* eslint-disable-next-line */
+    function getWidth(banner: IBanner): number | undefined {
+        /**
+         * Если с сервера пришёл "резиновый" баннер, то задаём ширину 100% через
+         * класс "ext-embed__ext-capirs_fill".
+         */
+        if (!isResponsiveAd(banner.width)) {
+            /**
+             * Если же пришёл баннер с фиксированной шириной, передаём её из iframe
+             * наверх.
+             */
+            return parseInt(banner.width, 10);
+        }
+    }
+
+    function getHeight(banner: IBanner): number {
+        if (!isResponsiveAd(banner.height)) {
+            return parseInt(banner.height, 10);
+        }
+
+        const { body, documentElement: html } = document;
+
+        /**
+         * Получаем высоту содержимого страницы
+         * @see https://stackoverflow.com/a/1147768/7200211
+         */
+        return Math.max(
+            body.scrollHeight,
+            body.offsetHeight,
+            html.clientHeight,
+            html.scrollHeight,
+            html.offsetHeight
+        );
+    }
+
+    window[globalCallbackProperty] = {
+        lib: {
+            init: () => {
+                const block = document.body.querySelector<HTMLDivElement>('.capirs-container');
+
+                window.Adf.banner.ssp(block, params.json, {
+                    'begun-auto-pad': params.begunAutoPad,
+                    'begun-block-id': params.begunBlockId
+                });
+            }
+        },
+        block: {
+            draw: (feed: IFeed) => {
+                const [banner] = feed.banners.graph;
+
+                window.parent.postMessage(
+                    {
+                        message: 'loading-succeed',
+                        width: getWidth(banner),
+                        height: getHeight(banner)
+                    },
+                    '*'
+                );
+            },
+            unexist: () => {
+                window.parent.postMessage({ message: 'loading-failed' }, '*');
+            }
+        }
+    };
+
+    const script = document.createElement('script');
+    script.src = '//ssp.rambler.ru/capirs_async.js';
+    script.async = true;
+    document.head.appendChild(script);
+}
