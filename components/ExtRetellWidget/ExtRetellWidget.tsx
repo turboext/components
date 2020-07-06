0bb590266f69a4a7ab9ba6c6f3a541f99779713c
sobolew <sobolew42@gmail.com>
2020-07-06 15:17:00 +0600

    changed widget parameters pass

--- components/ExtRetellWidget/ExtRetellWidget.tsx
+++ components/ExtRetellWidget/ExtRetellWidget.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import { camelCase } from 'camel-case';
 import { ExtEmbed } from '../ExtEmbed/ExtEmbed';
 
 interface IProps {
     'data-url': string;
     'data-source'?: string;
     'data-theme'?: string;
+    'data-rate'?: string | number;
 }
 
 interface IState {
@@ -17,6 +19,7 @@ interface IWidgetOptions {
     url: string;
     source?: string;
     theme?: string;
+    rate?: string | number;
 }
 
 enum LoadingState {
@@ -99,19 +102,22 @@ export class ExtRetellWidget extends React.PureComponent<IProps, IState> {
     }
 
     private makeInitializer(): string {
-        const {
-            'data-url': url,
-            'data-source': source,
-            'data-theme': theme
-        } = this.props;
-
-        const options: IWidgetOptions = {
-            url,
-            source,
-            theme
-        };
+        const options: IWidgetOptions = this.getWidgetOptions();
 
         return `Speechki.init(${JSON.stringify(options)})`;
+    }
+
+    private getWidgetOptions(): IWidgetOptions {
+        return Object.keys(this.props).reduce((prev, cur) => {
+            if (cur.indexOf('data-') < 0) {
+                return { ...prev };
+            }
+
+            return {
+                ...prev,
+                [camelCase(cur.replace('data-', ''))]: this.props[cur]
+            };
+        }, {} as unknown as IWidgetOptions);
     }
 
     private handlePostMessage(event: MessageEvent): void {
