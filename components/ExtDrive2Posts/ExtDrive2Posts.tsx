a57bb451cf77cb267d95eaea38fc86dceec0184a
Viacheslav Popov <anwerso@gmail.com>
2021-07-05 12:45:42 +0300

    Add "Show more" button for ExtDrive2Posts component

--- components/ExtDrive2Posts/ExtDrive2Posts.tsx
+++ components/ExtDrive2Posts/ExtDrive2Posts.tsx
@@ -22,11 +22,15 @@ interface IPost {
 
 interface IState {
     posts: IPost[];
+    isClosed: boolean;
 }
 
 export class ExtDrive2Posts extends React.Component<IProps, IState> {
+    public static CLOSED_POSTS_SIZE = 6;
+
     public state = {
-        posts: []
+        posts: [],
+        isClosed: true
     }
 
     private static renderPic(post: IPost): React.ReactNode {
@@ -49,23 +53,44 @@ export class ExtDrive2Posts extends React.Component<IProps, IState> {
         if (typeof window !== 'undefined') {
             fetch(source)
                 .then(res => res.json())
-                .then(data => this.setState({ posts: data.posts }))
+                .then(data => this.setState({
+                    posts: data.posts,
+                    isClosed: data.posts.length > ExtDrive2Posts.CLOSED_POSTS_SIZE
+                }))
                 .catch(() => {}); // eslint-disable-line no-empty-function
         }
     }
 
     public render(): React.ReactNode {
-        if (this.state.posts.length === 0) {
+        const { posts, isClosed } = this.state;
+        const size = isClosed ? ExtDrive2Posts.CLOSED_POSTS_SIZE : posts.length;
+
+        if (posts.length === 0) {
             return null;
         }
 
         return (
             <>
                 {this.props.children}
-                <div className="d2-posts">
-                    {this.state.posts.map(ExtDrive2Posts.renderPic)}
+                <div className={`d2-posts ${isClosed ? ' d2-posts--closed' : ''}`}>
+                    {posts.slice(0, size).map(ExtDrive2Posts.renderPic)}
                 </div>
+                {isClosed ?
+                    <button
+                        className="turbo-button turbo-button_theme_gray turbo-button_width_max turbo-button_size_m"
+                        onClick={this.handleClick} type="button"
+                    >
+                        Показать ещё
+                    </button> :
+                    null
+                }
             </>
         );
+    }
+
+    public handleClick = () => {
+        this.setState({
+            isClosed: false
+        });
     }
 }
